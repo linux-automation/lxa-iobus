3ead77a3b4a6c25508782bde916eb00add3fb72d
Florian Scherf <f.scherf@pengutronix.de>
2019-12-02 11:11:35 +0100

    setup: pep8 fixes; use find_packages
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "remotelab_io",
-    version = "0.1.0",
-    description = "CANOpen open control domain",
-    packages = ["remotelab_io"],
-    install_requires = ["canopen", "aiohttp"],
-    entry_points = {
+    name='remotelab_io',
+    version='0.1.0',
+    description='CANOpen open control domain',
+    packages=find_packages(),
+    install_requires=[
+        'canopen',
+        'aiohttp',
+    ],
+    entry_points={
         'console_scripts': [
             'remotelab_canopen = remotelab_io.domain:main',
             'remotelab_canopen_cmd = remotelab_io.client:main'
         ]
-
     },
 )
