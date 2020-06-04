01d9508e0a9ac80154e1651ad1052fceb5fe9039
Florian Scherf <f.scherf@pengutronix.de>
2020-06-04 16:40:49 +0200

    setup: fix wrong bitrate on can setup
    Signed-off-by: Florian Scherf <f.scherf@pengutronix.de>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo ip link set can0 type can bitrate 125000
+sudo ip link set can0 type can bitrate 100000
 sudo ip link set up dev can0
-sudo ip link set can1 type can bitrate 125000
-sudo ip link set up dev can1
