03b18d5c00efb51c3eb33bc2fc310a7bb987ca5d
Jonas Martin <j.martin@pengutronix.de>
2020-10-05 16:03:08 +0200

    Firmware added: ethmux-S01 0.2.0, lxatac_can_io-t01 0.3.0, ptxtac-S03_CAN_GPIO 0.3.0

--- lxa_iobus/lpc11xxcanisp/firmware/versions.py
+++ lxa_iobus/lpc11xxcanisp/firmware/versions.py
@@ -1,12 +1,20 @@
 import os
 
-from lxa_iobus.server.node_drivers import PTXIOMuxDriver
+from lxa_iobus.server.node_drivers import PTXIOMuxDriver, IOMuxDriver, EthMuxDriver
 
 FIRMWARE_DIR = os.path.dirname(__file__)
 
 FIRMWARE_VERSIONS = {
     PTXIOMuxDriver: (
-        (0, 1, 0),
+        (0, 3, 0),
         os.path.join(FIRMWARE_DIR, 'ptxtac-S03_CAN_GPIO.bin'),
+    ),
+    IOMuxDriver: (
+        (0, 3, 0),
+        os.path.join(FIRMWARE_DIR, 'lxatac_can_io-t01.bin'),
+    ),
+    EthMuxDriver: (
+        (0, 2, 0),
+        os.path.join(FIRMWARE_DIR, 'ethmux-S01.bin'),
     ),
 }
