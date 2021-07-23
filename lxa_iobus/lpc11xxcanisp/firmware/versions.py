d5b78b095df03715147856d250dcb20499971384
Chris Fiege <cfi@pengutronix.de>
2021-07-23 14:57:26 +0200

    Firmware: Bump to Version 0.5.0
    Changelog for EthernetMux und Iobus4Do3Di3Ai Firmware:
    > ## [0.5.0] 23.07.2021
    >
    > ### Changed
    >  * CCAN Status interrupts are now handled and status informations is printed to the serial.
    >  * The CCACN controller is set back into operational mode 100ms after going into bus off.
    >  * CAN: The sample point has been moved from 87.5% to 75% to increase tolerance to phase noise.
    >    Now using: tq: 20, seg1: 14, seg2: 5, sjw: 4.
    >  * SDO accesses have been internally reordered. Now all SDO accesses are logged to the serial.
    >
    > ### Added
    >  * There is now a timeout that resets the `node_id` if the node did not get any SDO request for 30 seconds.
    >    To disable the timeout write 0 to SDO 0x2d06:0x01.
    
    Signed-off-by: Chris Fiege <cfi@pengutronix.de>

--- lxa_iobus/lpc11xxcanisp/firmware/versions.py
+++ lxa_iobus/lpc11xxcanisp/firmware/versions.py
@@ -10,11 +10,11 @@ FIRMWARE_VERSIONS = {
         os.path.join(FIRMWARE_DIR, 'ptxtac-S03_CAN_GPIO.bin'),
     ),
     Iobus4Do3Di3AiDriver: (
-        (0, 4, 0),
+        (0, 5, 0),
         os.path.join(FIRMWARE_DIR, 'lxatac_can_io-t01.bin'),
     ),
     EthernetMuxDriver: (
-        (0, 3, 0),
+        (0, 5, 0),
         os.path.join(FIRMWARE_DIR, 'ethmux-S01.bin'),
     ),
 }
