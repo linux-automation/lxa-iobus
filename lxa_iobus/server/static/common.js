fd945febe9964e6fd2047dc685209bbc149b94b4
Leonard Göhrs <l.goehrs@pengutronix.de>
2026-07-24 14:39:42 +0200

    web: add new dependency-free node detail view
    This is reachable from the node list and provides an overview of the
    inputs and outputs.
    It also relies on `EventSource` for live updates.
    
    Signed-off-by: Leonard Göhrs <l.goehrs@pengutronix.de>

--- lxa_iobus/server/static/common.js
+++ lxa_iobus/server/static/common.js
@@ -1,5 +1,22 @@
 "use strict";
 
+function array_equal(a, b) {
+  return a.length === b.length && a.every((val, index) => val === b[index]);
+}
+
+async function post_json(url, content) {
+  const body = JSON.stringify(content);
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+
+  await fetch(url, {
+    body: body,
+    headers: headers,
+    method: "POST",
+  });
+}
+
 function set_server_info(data) {
   data["can_interface_is_up"] = data["can_interface_is_up"] ? "UP" : "DOWN";
   data["can_tx_error"] = data["can_tx_error"] ? "TX_ERROR!" : "";
